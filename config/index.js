ee8de82553fd06c355362937dd7cc8fb0160eb53
zjtaozjtw <zjtaozjtw@Outlook.com>
2022-09-06 22:06:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -43,6 +43,20 @@ export const config = {
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://www.bilibili.com/"
     }, 
+    {
+      // 想要发送的人的名字
+      name: "自己",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "o3G3a5_ZVshL3ZqoNSSIcTSFyvY4",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "WmxJ2e2OFPo2pL-7clrglYKcC9BfeZfEuT05l8vrlsc",
+      // 所在省份
+      province: "湖北",
+      // 所在城市
+      city: "黄冈",
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://www.bilibili.com/"
+    }, 
   ],
 
   /**
